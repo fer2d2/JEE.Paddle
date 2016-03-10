ed11d65123313d1cb3a486f895db9c03aefa7bb0
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2016-03-10 19:43:16 +0100

    Added tests for Token expiration

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -1,9 +1,12 @@
 package data.entities;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Calendar;
+import java.util.Date;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import data.entities.Token;
@@ -11,11 +14,33 @@ import data.entities.User;
 
 public class TokenTest {
 
+    private User user;
+
+    private Token token;
+
+    @Before
+    public void initialize() {
+        user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        token = new Token(user);
+    }
+
     @Test
     public void testTokenUser() {
-        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
     }
 
+    @Test
+    public void testTokenExpired() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1970, 1, 1);
+        token.setCreatedDatetime(calendar);
+        assertTrue(token.hasExpired());
+    }
+
+    @Test
+    public void testTokenNotExpired() {
+        Calendar calendar = Calendar.getInstance();
+        token.setCreatedDatetime(calendar);
+        assertFalse(token.hasExpired());
+    }
 }
