c9f3adb1930a16079011b97f4c2d4525e12bcb2f
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2016-03-20 21:39:05 +0100

    dev: refactoring

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import java.util.Calendar;
-import java.util.Date;
-
 import org.junit.Before;
 import org.junit.Test;
 
