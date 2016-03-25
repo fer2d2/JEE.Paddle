285a3fc1b8054d0d52ff63992341be09146a40a0
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2016-03-25 16:57:25 +0100

    wip: added stub for TrainingResource tests

---
+++ src/test/java/api/TrainingResourceFunctionalTesting.java
@@ -0,0 +1,104 @@
+package api;
+
+import org.junit.Test;
+
+public class TrainingResourceFunctionalTesting {
+    
+    RestService restService = new RestService();
+    
+    // createTraining
+    
+    @Test
+    public void testCreateTrainingUnauthorized() {
+    }
+    
+    @Test
+    public void testCreateTraining() {
+    }
+    
+    @Test
+    public void testCreateTrainingInvalidUser() {
+    }
+    
+    @Test
+    public void testCreateTrainingInvalidCourt() {
+    }
+    
+    // showTraining
+    
+    @Test
+    public void testShowTrainings() {
+    }
+    
+    @Test
+    public void testShowTraining() {
+    }
+    
+    @Test
+    public void testShowInvalidTraining() {
+    }
+    
+    // updateTraining
+    
+    @Test
+    public void testUpdateTraining() {
+    }
+    
+    @Test
+    public void testUpdateTrainingInvalidTraining() {
+    }
+    
+    @Test
+    public void testUpdateTrainingInvalidUser() {
+    }
+    
+    @Test
+    public void testUpdateTrainingInvalidCourt() {
+    }
+    
+    // deleteTraining
+    
+    @Test
+    public void testDeleteTraining() {
+    }
+    
+    @Test
+    public void testDeleteTrainingInvalidTraining() {
+    }
+    
+    // addTrainee
+    
+    @Test
+    public void testAddTrainee() {
+    }
+    
+    @Test
+    public void testAddTraineeInvalidTraining() {
+    }
+    
+    @Test
+    public void testAddTraineeInvalidUser() {
+    }
+    
+    @Test
+    public void testAddTraineeInvalidUserRole() {
+    }
+    
+    // deleteTrainee
+    
+    @Test
+    public void testDeleteTrainee() {
+    }
+    
+    @Test
+    public void testDeleteTraineeInvalidTraining() {
+    }
+    
+    @Test
+    public void testDeleteTraineeInvalidUser() {
+    }
+    
+    @Test
+    public void testDeleteTraineeInvalidUserRole() {
+    }
+}
