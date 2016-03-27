92fb82230e05cc83436127ab588411a5bc8987e8
Fernando Moro Hernández <morohernandez.fernando@gmail.com>
2016-03-27 21:11:08 +0200

    Added repository methods to remove reservations in training dates and check if training exists for a given reservation date

--- src/main/java/data/daos/TrainingDaoImpl.java
+++ src/main/java/data/daos/TrainingDaoImpl.java
@@ -1,7 +1,11 @@
 package data.daos;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import data.entities.Reserve;
 import data.entities.Training;
 import data.entities.User;
 
@@ -13,6 +17,9 @@ public class TrainingDaoImpl implements TrainingExtended {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private ReserveDao reserveDao;
+
     @Override
     public boolean deleteByTrainingId(int id) {
         Training training = trainingDao.findById(id);
@@ -23,13 +30,6 @@ public class TrainingDaoImpl implements TrainingExtended {
 
         trainingDao.delete(training);
         return true;
-    }
-
-    @Override
-    public boolean findByDatetime() {
-        // TODO para ser utilizado en un servicio que pueda comprobar si
-        // existe la posibilidad de añadir una reserva.
-        return false;
     }
 
     @Override
@@ -65,5 +65,51 @@ public class TrainingDaoImpl implements TrainingExtended {
 
         trainingDao.save(training);
         return true;
+    }
+
+    @Override
+    public void deleteReservesMathingTraining(Calendar startDatetime, Calendar endDatetime) {
+        List<Reserve> reserves = reserveDao.findByDateBetween(startDatetime, endDatetime);
+
+        int trainingDayOfWeek = startDatetime.get(Calendar.DAY_OF_WEEK);
+        int trainingHour = startDatetime.get(Calendar.HOUR_OF_DAY);
+        int trainingMinutes = startDatetime.get(Calendar.MINUTE);
+
+        for (Reserve reserve : reserves) {
+            Calendar reserveDate = reserve.getDate();
+            int reserveDayOfWeek = reserveDate.get(Calendar.DAY_OF_WEEK);
+            int reserveHour = reserveDate.get(Calendar.HOUR_OF_DAY);
+            int reserveMinutes = reserveDate.get(Calendar.MINUTE);
+
+            if (reserveDayOfWeek == trainingDayOfWeek && reserveHour == trainingHour && reserveMinutes == trainingMinutes) {
+                reserveDao.delete(reserve);
+            }
+        }
+    }
+    
+    @Override
+    public boolean existsTrainingClassForDay(Calendar day) {
+        List<Training> trainings = trainingDao.findAll();
+        
+        int dayOfWeek = day.get(Calendar.DAY_OF_WEEK);
+        int hour = day.get(Calendar.HOUR_OF_DAY);
+        int minutes = day.get(Calendar.MINUTE);
+        
+        for(Training training : trainings) {
+            Calendar startDatetime = training.getStartDatetime();
+            Calendar endDatetime = training.getEndDatetime();
+            
+            int trainingDayOfWeek = startDatetime.get(Calendar.DAY_OF_WEEK);
+            int trainingHour = startDatetime.get(Calendar.HOUR_OF_DAY);
+            int trainingMinutes = startDatetime.get(Calendar.MINUTE);
+            
+            if(startDatetime.getTimeInMillis() <= day.getTimeInMillis() && day.getTimeInMillis() <= endDatetime.getTimeInMillis()) {
+                if (dayOfWeek == trainingDayOfWeek && hour == trainingHour && minutes == trainingMinutes) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
     }
 }
--- src/main/java/data/daos/TrainingExtended.java
+++ src/main/java/data/daos/TrainingExtended.java
@@ -1,13 +1,16 @@
 package data.daos;
 
+import java.util.Calendar;
+
 public interface TrainingExtended {
 
     public boolean deleteByTrainingId(int id);
-
-    public boolean findByDatetime();
 
     public boolean addTrainee(int trainingId, int traineeId);
 
     public boolean deleteTrainee(int trainingId, int traineeId);
 
+    public void deleteReservesMathingTraining(Calendar startDatetime, Calendar endDatetime);
+    
+    public boolean existsTrainingClassForDay(Calendar day);
 }
